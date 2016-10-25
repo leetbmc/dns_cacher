9cc4e8991c575fb87e14295408f51ce9c9cc2a28
Brendan Coffey <bmc@section9.net>
2016-10-24 17:37:03 -0700

    Add method comments.
    Remove extraneous method.

--- src/cacher.js
+++ src/cacher.js
@@ -1,17 +1,15 @@
-function gotItem(item) {
-  if (chrome.runtime.lastError) {
-    alert(chrome.runtime.lastError);
-  } else {
-    alert(item);
-  }
-}
-
+/*
+ * Given a valid URI, extract the hostname
+ */
 function getHostFromURI(uri) {
   var parser = document.createElement("a");
   parser.href = uri;
   return parser.hostname;
 }
 
+/*
+ * Validate an IPv4 formatted address
+ */
 function isValidIpV4Address(ipaddress)
 {
   if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress))
@@ -21,6 +19,21 @@ function isValidIpV4Address(ipaddress)
   return (false)
 }
 
+/*
+ * Add a host/ip pair to the table. This function will initialize the table if
+ * it is empty.
+ *
+ * The table format is:
+  {
+    'hostname': {
+      222.222.222.222: true,
+      111.111.111.111: true
+    },
+    'hostname2': {
+      99.99.99.99: true
+    }
+  }
+ */
 function addIp(host, ip) {
   chrome.storage.local.get(
     {host_ips: {}},
@@ -38,6 +51,10 @@ function addIp(host, ip) {
   );
 }
 
+/*
+ * On a successfully completed request, log the IP of the host which responded
+ * to the request. Ignores IPv6 addresses.
+ */
 chrome.webRequest.onCompleted.addListener(
   function(info) {
     var ip = info.ip;
